e87327bf726c8b0acd13316d35d0334d2d0c223e
Abner Trindade <abnertri02@gmail.com>
2025-07-08 11:17:52 -0300

    Add rota para Agendamentos

--- Vibers_Barbearia/frontend/agendamento-vibers/src/App.jsx
+++ Vibers_Barbearia/frontend/agendamento-vibers/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';
+import Agendamento from './pages/Agendamento/Agendamento';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/agendamento" element={<Agendamento />} />
       </Routes>
     </BrowserRouter>
   );
