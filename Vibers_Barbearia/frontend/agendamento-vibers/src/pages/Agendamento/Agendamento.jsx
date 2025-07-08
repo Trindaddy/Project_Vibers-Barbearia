b7f8883b8dd5423f23b8ecf9776a617a2e6042fc
Abner Trindade <abnertri02@gmail.com>
2025-07-08 11:15:06 -0300

    Estrutura do formulário

---
+++ Vibers_Barbearia/frontend/agendamento-vibers/src/pages/Agendamento/Agendamento.jsx
@@ -0,0 +1,64 @@
+import "./Agendamento.css";
+import { useNavigate } from "react-router-dom";
+
+function Agendamento() {
+  const navigate = useNavigate();
+
+  const handleCancelar = () => {
+    navigate("/"); // Volta para a LandingPage
+  };
+
+  const handleContinuar = () => {
+    // Aqui você pode adicionar validação ou redirecionar para a próxima etapa
+    alert("Agendamento continuado!"); // Apenas exemplo
+  };
+
+  return (
+    <div className="pagina-agendamento">
+      <div className="landing-container">
+        <div className="header">
+          <h3>Viber’s Barbearias</h3>
+        </div>
+
+        <div className="main-content">
+          <h2>Agende o seu horário</h2>
+          <p>
+            Agende seu horário com facilidade! Basta selecionar o melhor
+            horário e pronto!
+          </p>
+        </div>
+
+        <form>
+          <label htmlFor="nome">Nome Completo</label>
+          <input type="text" id="nome" placeholder="Seu nome" />
+
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" placeholder="exemplo@company.com" />
+
+          <label htmlFor="telefone">Telefone</label>
+          <input type="tel" id="telefone" placeholder="61 91234 5678" />
+
+          <label htmlFor="data">Data</label>
+          <input type="text" id="data" placeholder="dd/mm/aaaa" />
+
+          <label htmlFor="horario">Horario</label>
+          <input type="text" id="horario" placeholder="Selecione o seu horário" />
+        </form>
+
+        <div className="botao-container">
+          <button onClick={handleCancelar}>
+            Cancelar
+            <span className="material-symbols-outlined">arrow_forward</span>
+          </button>
+
+          <button onClick={handleContinuar}>
+            Continuar
+            <span className="material-symbols-outlined">arrow_forward</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Agendamento;
