803804a2e462c2c8eb923d977a787d7fa5253227
Abner Trindade <abnertri02@gmail.com>
2025-07-29 19:24:05 -0300

    Add rotas para painel do admin e dos agendamentos do admin

--- Vibers_Barbearia/frontend/agendamento-vibers/src/App.jsx
+++ Vibers_Barbearia/frontend/agendamento-vibers/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';
 import Agendamento from './pages/Agendamento/Agendamento';
 import Confirmacao from './pages/Confirmacao/Confirmacao';
+import AdminPainel from './pages/AdminPainel/AdminPainel.jsx';
+import Agendamentos from './pages/AdminPainel/Agendamentos';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/agendamento" element={<Agendamento />} />
         <Route path="/confirmacao" element={<Confirmacao />} />
+        <Route path="/admin" element={<AdminPainel />} />
+        <Route path="/admin/agendamentos" element={<Agendamentos />} />
       </Routes>
     </BrowserRouter>
   );
