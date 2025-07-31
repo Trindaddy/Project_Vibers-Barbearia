2a62d7dd08459fd0bda89c184424caa9b8d3f7ac
Abner Trindade <abnertri02@gmail.com>
2025-07-31 20:42:50 -0300

    Ajuste

--- Vibers_Barbearia/backend/api/app.py
+++ Vibers_Barbearia/backend/api/app.py
@@ -3,9 +3,9 @@ from flask_cors import CORS
 from agendamentos import agendamento_bp  
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:5173"])
+CORS(app, origins=["http://localhost:5173"])  # ou CORS(app) para liberar todas origens
 
-app.register_blueprint(agendamento_bp)
+app.register_blueprint(agendamento_bp, url_prefix="/api")  # Prefixo importante para o fetch do frontend
 
 if __name__ == "__main__":
     app.run(debug=True)
