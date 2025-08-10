695c7c291ca38999af8cc8e560410c6a8d25d008
Abner Trindade <abnertri02@gmail.com>
2025-08-09 23:34:46 -0300

    feat(backend): configurar aplicativo para autenticação JWT e uploads

--- Vibers_Barbearia/backend/app.py
+++ Vibers_Barbearia/backend/app.py
@@ -3,23 +3,26 @@ from flask import Flask, send_from_directory
 from flask_cors import CORS
 from agendamentos import agendamento_bp
 from configuracoes import config_bp
+from auth import auth_bp
 from database.database import get_db_connection
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default-fallback-secret-key')
 CORS(app, origins=["http://localhost:5173"])
 
-# Define a pasta de uploads
 UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
 
 app.register_blueprint(agendamento_bp, url_prefix="/api")
 app.register_blueprint(config_bp, url_prefix="/api")
+app.register_blueprint(auth_bp, url_prefix="/api")
 
-# --- NOVA ROTA PARA SERVIR AS IMAGENS ---
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
